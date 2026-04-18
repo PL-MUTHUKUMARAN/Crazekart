936ed36e833490ae862849ccf2816578b07f05d2
Muthu Kumaran <plmuthukumaran@gmail.com>
2026-04-18 08:59:42 +0530

    Removed unique constraint from username

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
